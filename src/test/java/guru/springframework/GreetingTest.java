371461288d0e36055ea1e4318b3f536a97be0045
bentagtow <94013701+bentagtow@users.noreply.github.com>
2021-12-07 15:39:29 -0600

    completes maven setup lesson 40

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,20 +1,43 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
+    private Greeting greeting;
 
+    @BeforeAll
+    public static void beforeClass(){
+        System.out.println("BeforeAll - should only get called one");
+    }
+
+    @BeforeEach
+    void setUp(){
+        System.out.println("in before each... ");
+        greeting = new Greeting();
+    }
     @Test
     void helloWorld() {
-        Greeting greeting = new Greeting();
         System.out.println(greeting.helloWorld());
     }
 
     @Test
-    void testHelloWorld() {
-        Greeting greeting = new Greeting();
+    void helloWorld1() {
         System.out.println(greeting.helloWorld("Ben"));
+    }
+    @Test
+    void helloWorld2() {
+        System.out.println(greeting.helloWorld("Mitch"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("in after each... ");
+    }
+
+    @AfterAll
+    public static void afterClass(){
+        System.out.println("AFTER All - should only get called one");
     }
 }
